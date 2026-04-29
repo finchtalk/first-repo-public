90718498d1b2ecd4fb858c1fc490bf045403bf7a
Hemanth Reddy <HemanthReddy1782>
2026-04-29 21:15:49 +0530

    my first code

---
+++ Hemanth.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+Class main {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in)
+        System.out.println("Enter The name : ")
+        String name = sc.next();
+
+        System.outprintln(name);
+    }
+}
